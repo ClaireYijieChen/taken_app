6329b131a189690124d0e4845c1831815b5c5a9d
Joshua Morton <joshua.morton13@gmail.com>
2013-12-23 21:48:41 -0500

    all notes now shows something useful, like all of the notes

---
+++ notes/templatetags/getNotes.py
@@ -0,0 +1,17 @@
+from django import template
+import os, os.path
+
+register = template.Library()
+
+
+@register.filter
+def getNoteBody(noteLocation):
+    if os.getcwd()[-7::] != "library":
+        os.chdir("library")
+    location = noteLocation
+    f = open(location, "r")
+    noteBody = f.read()
+    f.close()
+    os.chdir('..')
+    return noteBody
+
